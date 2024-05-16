7c7ecc2d8d3d6a2daa7b2ec354ac17887b356cde
shravanihiroji <168087756+shravanihiroji@users.noreply.github.com>
2024-05-16 12:10:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,119 @@
+import streamlit as st
+from PyPDF2 import PdfReader ##to read all the documents
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain_community.vectorstores import FAISS
+#from langchain.vectorstores import FAISS  ##FAISS for vector embeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+
+load_dotenv() ##loading the environment variable
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def initialize():
+  # configure api key and initialise model
+  if "GOOGLE_API_KEY" not in os.environ:
+      os.environ["GOOGLE_API_KEY"] = 'AIzaSyCfCiijTQgw8zRkDDFJuxZye5BomGlLagY'
+
+  genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+
+
+
+##create a function to create a text variable to read the pdf pages 
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)  ##to read the specific PDF
+        for page in pdf_reader.pages:  ##extract text from multiple pages which is in a list and create one single string
+            text+= page.extract_text()
+    return  text
+
+
+
+##Divide the text into chunks
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+
+##convert the text chunks into vectors
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001") ##embedding 001 is a embedding technique
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings) ##embedding all the text using the embedding technique
+    vector_store.save_local("faiss_index")  ##Save this information in local inside the folder faiss_index
+
+
+##Defining Prompt
+def get_conversational_chain():
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"]) ##Input to the prompt template
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt) #chain type = stuff helps to do internal text summarization
+
+    return chain
+
+
+
+##For User Question
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+
+
+##Creating StreamLit App
+def main():
+    
+    st.set_page_config("Chat with Multiple PDF")
+    st.header("Chat with Multiple PDF using Gemini")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
